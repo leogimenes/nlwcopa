c9ce17d0ed9e5093a8f393e9c5e84f1cbb28e398
Leonardo Gimenes <leonardo.gimenes@lbca.com.br>
2022-11-05 13:33:10 -0300

    push concluded nlw-copa project

---
+++ server/prisma/seed.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+async function main() {
+
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      avatarUrl: 'https://github.com/leogimenes.png'
+    }
+  })
+
+  const poll = await prisma.poll.create({
+    data: {
+      title: 'Example Poll',
+      code: 'BOL123',
+      ownerId: user.id,
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-04T12:00:00.430Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR'
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-05T12:00:00.430Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_pollId: {
+                userId: user.id,
+                pollId: poll.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+  // const participant = await prisma.participant.create({
+  //   data: {
+  //     pollId: poll.id,
+  //     userId: user.id
+  //   }
+  // })
+}
+
+main();
---
+++ web/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app';
+import '../styles/global.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps}></Component>
+}
+
+export default MyApp;
